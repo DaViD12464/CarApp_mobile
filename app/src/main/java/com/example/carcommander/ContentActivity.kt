c7152c50a56c4f2634be65f5c86a083292288d13
TymoteuszBroda <Tymoteusz.91100@gmail.com>
2024-05-23 17:44:43 +0200

    finished placeholder grid/card scrollable layout for reference

--- app/src/main/java/com/example/carcommander/ContentActivity.kt
+++ app/src/main/java/com/example/carcommander/ContentActivity.kt
@@ -1,9 +1,15 @@
 package com.example.carcommander
 
+import android.media.Image
 import android.os.Bundle
 import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 
+data class carData(
+    val image: Image,
+    val brand: String,
+    val model: String
+)
 class ContentActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?) {
